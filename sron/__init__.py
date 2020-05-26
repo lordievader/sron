947a3f8c9c1538dfd723a96dcded05840f5e9e84
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2020-05-26 10:09:03 +0200

    Add definitions for the 'Bright' colorscheme
    Allow easy access to the different colors.

--- sron/__init__.py
+++ sron/__init__.py
@@ -1,3 +1,11 @@
 """Boilerplate for sron library.
 """
 from sron.sron import Colormap, BrightColormap, VibrantColormap, MutedColormap
+
+blue = '#4477AA'
+cyan = '#66CCEE'
+green = '#228833'
+yellow = '#CCBB44'
+red = '#EE6677'
+purple = '#AA3377'
+grey = '#BBBBBB'
